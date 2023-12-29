20047fc7c2c8896d415b2c23a935b4fe57aa13e7
Rohit kumar <104249513+rohitkumar1302@users.noreply.github.com>
2023-12-29 16:14:32 +0530

    Add files via upload

---
+++ naya.php
@@ -0,0 +1,9 @@
+<?php
+
+echo " <h1>1. INTRODUCTION</h1>
+<h2>1. Project DescriptionThis project is designed so as to be used by Bike Rental Company specializing in rentingbikes to customers. It is an online system through which customers can view availablebikes, register, view profile and book bike.1.2 ObjectiveThe   advancement   in   Information   Technology   and   internet   penetration   has   greatlyenhanced various business processes and communication between companies  servicesprovider and their customers of which bike rental industry is not left out. This E-BikeRental system is developed to provide the following services•Enhance Business Processes To be able to use internet technology to project the rentalcompany to the global world instead of limiting their services to their local domain alone,thus increase their return on investment</h2>
+
+" ;
+
+
+?>
